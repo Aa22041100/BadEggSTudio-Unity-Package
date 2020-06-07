f8259d834adf8ad6adcfdfb2afcfa0aa23679c09
Stanley Lai <stanley@lknstanley.com>
2020-06-08 00:42:03 +0100

    -Added the first version of item database framework - Includes base item, base bonus, item database and item database manager. - Includes demo scene

---
+++ Assets/BadEggStudio Package/BadEggStudio/Collection/Base/BaseBonus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace BadEggStudio.Collection {
+
+    [Serializable]
+    public class BaseBonus
+    {
+        public float health;
+        public float mana;
+        public float atk;
+        public float def;
+    }
+
+}
---
+++ Assets/BadEggStudio Package/BadEggStudio/Collection/Container/ItemDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BadEggStudio.Collection {
+
+    [CreateAssetMenu(fileName = "Item_Database", menuName = "BadEggStudio/Items/Create Database", order = 1)]
+    public class ItemDatabase : ScriptableObject {
+        public List<BaseItem> items;
+    }
+}
